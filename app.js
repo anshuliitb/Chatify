75f0ba908c24854e6623405f9cc3e436527449ed
Anshul Mahor <anshulmahoriitb@gmail.com>
2025-07-08 01:56:28 +0530

    added link on console

--- app.js
+++ app.js
@@ -98,5 +98,5 @@ io.on("connection", async (socket) => {
 // Start server
 const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
-  console.log(`🚀 Server listening on http://localhost:${PORT}`);
+  console.log(`🚀 Server listening on http://13.61.12.131:${PORT}/`);
 });
